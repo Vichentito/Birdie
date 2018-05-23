baf98f157f08671d9cbe16baeb4b30aa087d37d4
Vichentito <chente54321@gmail.com>
2018-05-23 15:13:52 -0500

    aa

--- routes/apps.js
+++ routes/apps.js
@@ -10,6 +10,6 @@ var multipart = require('connect-multiparty');
 var md_upload = multipart({ uploadDir: './uploads/apks' });
 
 api.get('/probando-controlador-apks', Apps.pruebas);
-api.get('/get-image-users/:imageFile', Apps.getImageFile);
+api.get('/get-apk-file/:imageFile', Apps.getImageFile);
 
 module.exports = api;
